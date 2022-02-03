f29369c58eeb833e90413d8424a56a84fe495383
marcosbergami <marcos@mogri.com>
2022-02-02 21:49:25 -0600

    chore: refactoring application structure to enhance single responsibility principle

--- src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -1,7 +1,7 @@
 import e from "express";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { User } from "../../entities/User";
+import { User } from "../../infra/typeorm/entities/User";
 import { IUsersRepository } from "../IUsersRepository";
 
 class UsersRepositoryInMemory implements IUsersRepository {
